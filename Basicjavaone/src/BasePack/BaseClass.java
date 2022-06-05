f2bb0112a006f8c65e68ff64c66303762fa6653e
susmita <sushmitamali99@gmail.com>
2022-06-06 02:05:32 +0530

    third one

---
+++ Basicjavaone/src/BasePack/BaseClass.java
@@ -0,0 +1,14 @@
+package BasePack;
+
+public class BaseClass {
+	
+	public static void main() {
+		
+		
+		
+	}
+
+	public static void m1() {
+		System.out.println("I am from base class" );
+	}
+}
---
+++ Basicjavaone/src/BasePack/ChildClass.java
@@ -0,0 +1,15 @@
+package BasePack;
+
+public class ChildClass extends BaseClass{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChildClass obj = new ChildClass();
+		obj.m1();
+		
+	}
+	public static void m1() {
+		System.out.println("I am from child class");
+	}
+}
